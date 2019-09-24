3629b669bdbd9dd4df2db516ecd64bc3fec8f34a
supremeyh <supremeyh@126.com>
2019-09-24 23:45:13 +0800

    navigationOptions

--- App.js
+++ App.js
@@ -4,6 +4,16 @@ import {createAppContainer} from 'react-navigation';
 import {createStackNavigator} from 'react-navigation-stack';
 
 class HomeScreen extends Component {
+  static navigationOptions = {
+    title: 'Home',
+    headerStyle: {
+      backgroundColor: '#f4511e',
+    },
+    headerTintColor: '#fff',
+    headerTitleStyle: {
+      fontWeight: 'bold',
+    },
+  };
   render() {
     const {navigation} = this.props;
     return (
